8a3baa701dc21c6a0c456e2439bdcdaa7623423e
tshepi_tech <tshepi.tech@gmail.com>
2022-02-28 08:00:01 +0100

    portal created with reactdoM, CSS fixed to show portal and overlay

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,12 +1,16 @@
+import ReactDOM from "react-dom";
+
 export default function Modal({ showModal, children, onClose }) {
   if (!showModal) return null;
-  return (
+  return ReactDOM.createPortal(
     <>
-      <div className="overlay_styles" />
-      <div className="modal_styles">
-        <button onClick={onClose}>Close modal</button>
-        {children}
+      <div className="overlay_styles">
+        <div className="modal_styles">
+          <button onClick={onClose}>Close modal</button>
+          {children}
+        </div>
       </div>
-    </>
+    </>,
+    document.getElementById("portal")
   );
 }
