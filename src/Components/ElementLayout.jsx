2426d7fe878d05164575a8c155ce286039497090
tshepi_tech <tshepi.tech@gmail.com>
2022-03-01 22:31:31 +0100

    styling element layout

--- src/Components/ElementLayout.jsx
+++ src/Components/ElementLayout.jsx
@@ -2,10 +2,10 @@ export default function ElementLayout(element) {
   const { media, title, text } = element;
 
   return (
-    <>
-      <h1>{title}</h1>
-      <p className="description">{text}</p>
+    <div className="mainLayout">
+      <h2>{title}</h2>
+      <h3 className="description">{text}</h3>
       <div className="box">{media}</div>
-    </>
+    </div>
   );
 }
