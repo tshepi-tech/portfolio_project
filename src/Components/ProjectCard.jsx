eee14130df23f9d588f785e3828c5c046edf80e6
tshepi_tech <tshepi.tech@gmail.com>
2022-03-01 16:59:06 +0100

    techstack added to projects

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -4,13 +4,14 @@ export default function ProjectCard({ projects, shownProject }) {
   const heading = projectDisplayed.name;
   const description = projectDisplayed.description;
   const comment = projectDisplayed.comment;
+  const techStack = projectDisplayed.techStack;
 
   return (
     <>
       <h1>{heading}</h1>
       {projectDisplayed.uploaded && <h2>{description}</h2>}
       {!projectDisplayed.uploaded && <h2>{comment}</h2>}
-      <h3>techStack</h3>
+      <h3>{techStack}</h3>
       <button>Website</button>
       <button>Github repository</button>
     </>
