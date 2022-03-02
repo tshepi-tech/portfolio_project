1352904d8013edc99af5626013a37587f2bf8500
tshepi_tech <tshepi.tech@gmail.com>
2022-03-02 18:32:38 +0100

    display cards temporarily styled

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -3,7 +3,7 @@ import ElementLayout from "../Components/ElementLayout";
 import ProjectItem from "../Components/ProjectItem";
 
 const text =
-  "Have a look at my projects that I ahve done and some which will be uploaded throughout my journey! Don't hesitate to share your thoughts.";
+  "Have a look at my projects that I have done and some which will be uploaded throughout my journey! Don't hesitate to share your thoughts.";
 
 export default function Portfolio({
   projects,
@@ -25,7 +25,7 @@ export default function Portfolio({
     <div id="Portfolio">
       <h2 className="portfolio-h2">Portfolio</h2>
       <p className="portfolio-description">{text}</p>
-      <section className="displayCards-box ">{listProjects}</section>
+      <section className="container">{listProjects}</section>
       {/* <ElementLayout title="Portfolio" text={text} media={listProjects} /> */}
     </div>
   );
